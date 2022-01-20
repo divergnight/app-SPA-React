a83ce4e4fce1bdb39da6efa6de1419ba1e07395c
divergnight <47478344+divergnight@users.noreply.github.com>
2022-01-20 12:54:04 +0100

    Init commit

---
+++ src/components/AppNavbar/AppNavbar.jsx
@@ -0,0 +1,17 @@
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+export default function AppNavbar() {
+	return (
+		<Navbar bg="light" expand="lg">
+			<Container id="AppNavbar-nav-container">
+				<Navbar.Brand href="#home">App SPA</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link href="#home">Accueil</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
